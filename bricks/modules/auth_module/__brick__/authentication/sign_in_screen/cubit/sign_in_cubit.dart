ee14c0705c9b0b3ced82361ab06f135ae0d9c38b
MBanaszynski <maciej.banaszynski99@gmail.commaciej.banaszynski99@gmail.commaciej.banaszynski99@gmail.com>
2023-12-28 19:22:12 +0100

    Remove unwanted code

--- bricks/modules/auth_module/__brick__/authentication/sign_in_screen/cubit/sign_in_cubit.dart
+++ bricks/modules/auth_module/__brick__/authentication/sign_in_screen/cubit/sign_in_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_presentation/bloc_presentation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'sign_in_cubit_event.dart';
 
 part 'sign_in_cubit.freezed.dart';
 part 'sign_in_state.dart';
 
-class SignInCubit extends Cubit<SignInState> {
+class SignInCubit extends Cubit<SignInState>
+    with BlocPresentationMixin<SignInState, SignInCubitEvent> {
   SignInCubit() : super(const SignInState.initial());
 }
--- bricks/modules/auth_module/__brick__/authentication/sign_up_screen/sign_up_view.dart
+++ bricks/modules/auth_module/__brick__/authentication/sign_up_screen/sign_up_view.dart
@@ -1,18 +1,8 @@
 import 'package:bloc_presentation/bloc_presentation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
-import '../../../common/extensions/grow_snack_bar_on_context.dart';
-import '../../../common/extensions/localization_on_build_context.dart';
-import '../../../common/widgets/app_bar/grow_app_bar.dart';
-import '../login_screen/login_screen.dart';
-import '../sign_in_screen/sign_in_screen.dart';
-import '../widgets/onboarding_header_text.dart';
 import 'cubit/sign_up_cubit.dart';
 import 'cubit/sign_up_cubit_event.dart';
-import 'widgets/sign_up_button_section.dart';
-import 'widgets/sign_up_form_section.dart';
-import 'widgets/terms_and_conditions_section.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({Key? key}) : super(key: key);
@@ -41,19 +31,17 @@ class SignUpView extends StatelessWidget {
   void onBlocListenerEvent(BuildContext context, SignUpCubitEvent? event) {
     switch (event) {
       case AccountAlreadyExists():
-        context.showErrorSnackBar(
-          title: context.localizations.signUpPageAccountAlreadyExistsExceptionHeader,
-          body: context.localizations.signUpPageAccountAlreadyExistsExceptionBody,
-          onBodyLinkTapped: () => Modular.to.pushNamed(SignInScreen.path),
-        );
+        //TODO: Implement AccountAlreadyExists error
+        throw UnimplementedError();
       case SignUpFailed():
-        context.showDefaultErrorSnackbar();
+        //TODO: Implement SignUpFailed error
+        throw UnimplementedError();
       case TooManyRequests():
-        context.showErrorSnackBar(
-            title: context.localizations.snackbarDefaultExceptionHeader,
-            body: context.localizations.signUpTooManyRequestsSnackbarBody);
+        //TODO: Implement TooManyRequests error
+        throw UnimplementedError();
       default:
-        context.showDefaultErrorSnackbar();
+        //TODO: Implement Default and other cases
+        throw UnimplementedError();
     }
   }
 }
